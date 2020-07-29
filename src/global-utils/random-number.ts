ee23d8033aa909b0c5e6198449dda3230fda1e8e
eddie naff <eddienaff@eddienaff.com>
2020-07-29 11:43:06 -0700

    Add random number utility

--- src/global-utils/random-number.ts
+++ src/global-utils/random-number.ts
@@ -1,3 +1,7 @@
 export function randomNumber(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
+
+export function randomInteger(min: number, max: number): number {
+  return Math.round(Math.random() * (max - min) + min);
+}
