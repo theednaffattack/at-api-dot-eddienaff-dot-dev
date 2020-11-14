689327309ebb7d812608b1462056e6adf128f062
Eddie Naff <eddie@eddienaff.dev>
2020-11-13 18:49:29 -0800

    Remove comments

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["dotenv/config"],
-  // setupFilesAfterEnv: [
-  //   // NOT setupFiles
-  //   "./src/test-utils/default-timeout.js",
-  // ],
   // testUrl: "",
   roots: ["__tests__"],
   // globalSetup: "./jest-config/global-setup.js"
